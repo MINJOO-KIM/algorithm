9ed337a47e6acf48fe0f64a5d7af5abb0ef1bd8f
MINJOO-KIM <gkminju2000@gmail.com>
2023-03-09 01:50:15 +0900

    문제6

---
+++ 6.js
@@ -0,0 +1,16 @@
+let a = NaN;
+let b = 1;
+let c = "";
+let d = 0;
+let e = undefined;
+
+const trueOrFalse = (question) => {
+  if (question) {
+    console.log(question + " is true");
+  }
+};
+
+trueOrFalse(a);
+trueOrFalse(b);
+trueOrFalse(c);
+trueOrFalse(d);
